599a0652bec42434b6c8ef7d6b81e21a47f69a73
Arbe <bektemir1102@gmail.com>
2026-02-22 22:01:40 +0600

    dont know

--- core/pay/models.py
+++ core/pay/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from users.models import User
+# просто имитация!
 
-# Create your models here.
+class Card(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="orders",
+        null=True, blank=True,
+        verbose_name="Пользователь",
+    )
+    name = models.CharField(max_length=50)
+    number = models.IntegerField(
+        max_length=16,
+        unique=True, null=True, blank=True,
+    )
+    money = models.DecimalField(max_digits=10, decimal_places=2)
+    password = models.CharField
+    date = models.DateField()
+    cvs = models.PositiveSmallIntegerField(max_length=3)
+    email = models.EmailField()
+
+
