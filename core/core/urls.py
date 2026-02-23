2978aad1b4d10cc19ed0325122ef5d6f378135e6
Arbe <bektemir1102@gmail.com>
2026-02-23 20:00:49 +0600

    чуть дороботка ошибок

--- core/core/urls.py
+++ core/core/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     path('api/', include('shop.urls')),
     path('api/users/', include('users.urls')),
-    # path('api/orders/', include('orders.urls')),
+    path('api/orders/', include('orders.urls')),
     # path('api/pay/', include('pay.urls')),
 ]
 
