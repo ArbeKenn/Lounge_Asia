f72820ada6e27d9a7baa3d8abdd937dda2e6a5b4
Arbe <bektemir1102@gmail.com>
2026-02-23 19:59:56 +0600

    created files

--- core/pay/models.py
+++ core/pay/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 from users.models import User
 # просто имитация!
 
@@ -6,19 +8,25 @@ class Card(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name="orders",
+        related_name="card",
         null=True, blank=True,
         verbose_name="Пользователь",
     )
     name = models.CharField(max_length=50)
     number = models.IntegerField(
-        max_length=16,
-        unique=True, null=True, blank=True,
+        unique=True,
+        null=True,
+        blank=True,
+        validators=[MinValueValidator(1000000000000000), MaxValueValidator(9999999999999999)]
     )
     money = models.DecimalField(max_digits=10, decimal_places=2)
     password = models.CharField
     date = models.DateField()
-    cvs = models.PositiveSmallIntegerField(max_length=3)
+    cvs = models.IntegerField(
+        null=True,
+        blank=True,
+        validators=[MinValueValidator(1), MaxValueValidator(999)]
+    )
     email = models.EmailField()
 
 
